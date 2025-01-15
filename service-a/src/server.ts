1f17b98f89bf0c83c7aa15fb75ce9f169e1126d5
beerappabvgp <beerappabharathb@gmail.com>
2025-01-15 09:17:10 +0530

    Added dockerFile

--- service-a/src/server.ts
+++ service-a/src/server.ts
@@ -5,15 +5,24 @@ const redis = require("redis");
 const app = express();
 app.use(express.json());
 
+// const redisClient = redis.createClient({
+//     url: process.env.REDIS_URL || "redis://localhost:6397",
+// });
+
+const REDIS_HOST = process.env.REDIS_HOST || 'redis';
+const REDIS_PORT = process.env.REDIS_PORT || 6379;
+
+console.log(`Attempting to connect to Redis at ${REDIS_HOST}:${REDIS_PORT}`);
+
 const redisClient = redis.createClient({
-    url: process.env.REDIS_URL || "redis://localhost:6397",
+    url: `redis://${REDIS_HOST}:${REDIS_PORT}`,
 });
 
 redisClient.on("connect", () => {
     console.log("Service A connected to redis ... ");
 });
 
-redisClient.on("error", (err) => {
+redisClient.on("error", (err: any) => {
     console.error("redis error: ", err);
 });
 
@@ -22,7 +31,7 @@ redisClient.on("error", (err) => {
 })();   
 
 // API endpoint to produce or send data 
-app.post("/send", async (req, res) => {
+app.post("/send", async (req: any, res: any) => {
     const { channel, message } = req.body;
     if (!channel || !message) {
         return res.status(400).send({
